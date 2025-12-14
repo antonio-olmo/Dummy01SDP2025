90aa2a3cc4e67a0edfdd2dd8402034e090699c81
Antonio Olmo Ortiz <antonio@afosoft.com>
2025-12-14 20:03:24 +0100

    Initial Commit

---
+++ Dummy01SDP2025/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Dummy01SDP2025
+//
+//  Created by Antonio Olmo Ortiz on 14/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Dummy01SDP2025/Dummy01SDP2025App.swift
@@ -0,0 +1,17 @@
+//
+//  Dummy01SDP2025App.swift
+//  Dummy01SDP2025
+//
+//  Created by Antonio Olmo Ortiz on 14/12/25.
+//
+
+import SwiftUI
+
+@main
+struct Dummy01SDP2025App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
